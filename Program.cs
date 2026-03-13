6da432c3b53879e0ca0436e05cb4baaa4b586c7d
Seppmat <manikyosep73@gmail.com>
2026-03-13 16:08:56 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TableDriven
+        KodePos kodePos = new KodePos();
+
+        Console.Write("Masukkan kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        string kode = kodePos.getKodePos(kelurahan);
+        Console.WriteLine("Kode pos: " + kode);
+
+        Console.WriteLine();
+
+        //StateBased
+        DoorMachine pintu = new DoorMachine();
+
+        Console.WriteLine("Simulasi pintu:");
+        pintu.Buka();
+        pintu.Kunci();
+
+        Console.ReadLine();
+    }
+}
