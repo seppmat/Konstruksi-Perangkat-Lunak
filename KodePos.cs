17ffd9d49490d4f7023b27a71f5ccf73822321d6
Seppmat <manikyosep73@gmail.com>
2026-03-13 16:06:16 +0700

    Add Class KodePos

---
+++ KodePos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class KodePos
+{
+    private Dictionary<string, string> tabelKodePos = new Dictionary<string, string>()
+    {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        if (tabelKodePos.ContainsKey(kelurahan))
+        {
+            return tabelKodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan";
+        }
+    }
+}
