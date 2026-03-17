3efd2cd173f96038c7fed8d8c103a072a798552e
Seppmat <manikyosep73@gmail.com>
2026-03-18 01:59:25 +0700

    Add class KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class KodeBuah
+{
+    private Dictionary<string, string> kodeBuah = new Dictionary<string, string>()
+    {
+        {"Apel", "A00"},
+        {"Aprikot", "B00"},
+        {"Alpukat", "C00"},
+        {"Pisang", "D00"},
+        {"Paprika", "E00"},
+        {"Blackberry", "F00"},
+        {"Ceri", "H00"},
+        {"Kelapa", "I00"},
+        {"Jagung", "J00"},
+        {"Kurma", "K00"},
+        {"Durian", "L00"},
+        {"Anggur", "M00"},
+        {"Melon", "N00"},
+        {"Semangka", "O00"}
+    };
+
+    public string getKodeBuah(string namaBuah)
+    {
+        if (kodeBuah.ContainsKey(namaBuah))
+        {
+            return kodeBuah[namaBuah];
+        }
+        else
+        {
+            return "Kode tidak ditemukan";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        KodeBuah kb = new KodeBuah();
+
+        Console.Write("Masukkan nama buah: ");
+        string input = Console.ReadLine();
+
+        string kode = kb.getKodeBuah(input);
+        Console.WriteLine("Kode buah: " + kode);
+
+        Console.WriteLine();
+    }
+}
