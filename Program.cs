5dea0bde87e23adcc2ea864d90082a90d7cdff43
Seppmat <manikyosep73@gmail.com>
2026-04-05 13:24:32 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+        {
+            throw new ArgumentException("Judul tidak valid");
+        }
+
         Random random = new Random();
         this.id = random.Next(10000, 99999);
         this.title = title;
@@ -16,7 +21,22 @@ class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        this.playCount += count;
+        if (count < 0 || count > 10000000)
+        {
+            throw new ArgumentException("Input play count tidak valid");
+        }
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Terjadi overflow pada play count");
+        }
     }
 
     public void PrintVideoDetails()
@@ -31,9 +51,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Yosep");
-        video.IncreasePlayCount(100);
-        video.IncreasePlayCount(250);
-        video.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Yosep");
+
+            video.IncreasePlayCount(5000000);
+            video.IncreasePlayCount(7000000);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                video.IncreasePlayCount(10000000);
+            }
+
+            video.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
