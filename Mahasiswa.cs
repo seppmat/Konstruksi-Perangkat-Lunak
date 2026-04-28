d89a72708c4241d8ce73f773f9a5622ef17d7ff3
Seppmat <manikyosep73@gmail.com>
2026-04-28 23:31:51 +0700

    menambahkan API mahasiswa

---
+++ Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpModul9
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; } = "";
+        public string Nim { get; set; } = "";
+    }
+}
---
+++ MahasiswaController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpModul9
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        // Data static (tanpa database)
+        private static readonly List<Mahasiswa> mahasiswaList =
+        [
+            new Mahasiswa { Nama = "Yosep Warih Martua Manik", Nim = "103082400017" },
+        new Mahasiswa { Nama = "Muhamad Rozi", Nim = "103082400018" },
+        new Mahasiswa { Nama = "Daffa Surya Ramadhan", Nim = "103082400019" }
+        ];
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> GetMahasiswa()
+        {
+            return mahasiswaList;
+        }
+
+        // GET /api/mahasiswa/{index}
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetMahasiswaByIndex(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+
+            return mahasiswaList[index];
+        }
+
+        // POST /api/mahasiswa
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mhs)
+        {
+            mahasiswaList.Add(mhs);
+            return Ok();
+        }
+
+        // DELETE /api/mahasiswa/{index}
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+
+            mahasiswaList.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
