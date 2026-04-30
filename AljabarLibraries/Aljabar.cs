fbee4b2180e3de158c3b72c2db6e8c1aa4e3e1e7
Seppmat <manikyosep73@gmail.com>
2026-04-30 22:02:20 +0700

    implementasi library untuk dengan topik aljabar

---
+++ AljabarLibraries/Aljabar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        // Fungsi A: Akar Persamaan Kuadrat
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            // persamaan: {a, b, c}
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = (b * b) - (4 * a * c);
+
+            if (diskriminan < 0)
+            {
+                // tidak ada akar real
+                return new double[] { };
+            }
+
+            double x1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        // Fungsi B: Hasil Kuadrat ( (ax + b)^2 )
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            // persamaan: {a, b}
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double A = a * a;        // a^2
+            double B = 2 * a * b;    // 2ab
+            double C = b * b;        // b^2
+
+            return new double[] { A, B, C };
+        }
+    }
+}
---
+++ tpModul10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Contoh Akar Persamaan Kuadrat
+        double[] hasilAkar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+
+        Console.WriteLine("Akar Persamaan Kuadrat:");
+        foreach (var x in hasilAkar)
+        {
+            Console.WriteLine(x);
+        }
+
+        // Contoh Hasil Kuadrat
+        double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+
+        Console.WriteLine("\nHasil Kuadrat:");
+        Console.WriteLine($"{hasilKuadrat[0]}x^2 + {hasilKuadrat[1]}x + {hasilKuadrat[2]}");
+    }
+}
